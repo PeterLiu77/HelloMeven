9f9fe2f38ad29f95a51f73b997ccc0729ea7366d
PeterLiu77 <823807392@qq.com>
2018-06-27 15:37:22 +0800

    A new Project!

---
+++ src/main/java/com/peter/hello/HelloMeven.java
@@ -0,0 +1,9 @@
+package com.peter.hello;
+public class HelloMeven {
+
+    final String hhh="HHH";
+
+    public void sayHello() {
+        System.out.println("hello!");
+    }
+}
---
+++ src/test/java/com/peter/hello/HelloMevenTest.java
@@ -0,0 +1,14 @@
+package com.peter.hello;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HelloMevenTest {
+
+    @Test
+    public void sayHello() {
+        HelloMeven hello = new HelloMeven();
+        Assert.assertEquals("HHH",hello.hhh);
+        hello.sayHello();
+    }
+}
